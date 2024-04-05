7a996a732243d64b88fda52933ac42a263d89e83
grecpt <grecpt@gmail.com>
2024-04-06 09:17:40 +1000

    Adding comments to the index.js file

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
+//Loads the Express library for use in our code
 const express = require('express')
 
+//Create an instance of an Express app
 const app = express()
 
+//Our HTTP server will listen on port 3000.
 const port = 3000
 
+//Create a handler for the main HTTP route
 app.get('/', (req, res) => {
-
+    //This handler prints Hello World! in the web browser.
     res.send('Hello World!')
 
 });
-
+//Initiates the HTTP server
 app.listen(port, () => {
+    //The callback prints a message when the server has started.
     console.log(`Example app listening on port ${port}`);
 });
